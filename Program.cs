09c8bdfb2d41ea4dec2b7e061a2eaebc324a009a
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-11-16 17:38:01 +0400

    Work15

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("эй, это  не день недели");
+  }
+  else Console.WriteLine("разве,этот день не выходной -> нееет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
